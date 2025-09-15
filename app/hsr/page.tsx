65fc64273a653d105809b425bfd58f42eaffa69d
Andrea Figueroa <andrea.figue@gmail.com>
2025-09-15 14:02:15 -0700

    fix

--- app/hsr/page.tsx
+++ app/hsr/page.tsx
@@ -1,6 +1,5 @@
 // pages/index.tsx
 import React from 'react';
-import TravelComparison from '../components/hsr';
 import ChartComponent from '../components/hsr2';
 
 const HomePage: React.FC = () => {
